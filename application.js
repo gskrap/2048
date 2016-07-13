f8232bc762f804a2563cba2b7e9fb27ac43b078a
George Skrapits <gskrap@gmail.com>
2016-07-13 15:32:51 -0400

    generate random board

--- application.js
+++ application.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+
+  Game.prototype.populateDom = function() {
+    for (var i = 0; i < 16; i++) {
+      $("#"+String(i)).html(this.currentBoard[i])
+    }
+  }
+
+  g = new Game;
+  g.populateDom();
+
   $(document).on('keyup',function(e){
     if(e.which==38) {
       console.log("up");
@@ -13,4 +23,21 @@ $(document).ready(function() {
       console.log("left");
     };
   });
+
+  function Game() {
+    this.startingBoard = boardGenerator();
+    this.lastBoard = this.startingBoard;
+    this.currentBoard = this.startingBoard;
+  }
+
+  function boardGenerator() {
+    result = [];
+    var idx1 = Math.floor(Math.random() * 16);
+    var idx2 = Math.floor(Math.random() * 16);
+    for(var i = 0; i < 16; i ++) {
+      if (i == idx1 || i == idx2) { result.push(2); }
+      else { result.push(0); }
+    }
+    return result;
+  }
 });
