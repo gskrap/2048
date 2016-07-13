7933ecac2a5f5bb9be09a3790375c2584679ffaf
George Skrapits <gskrap@gmail.com>
2016-07-13 15:01:01 -0400

    log moves

--- application.js
+++ application.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  console.log("ready!")
+  $(document).on('keyup',function(e){
+    if(e.which==38) {
+      console.log("up");
+    };
+    if(e.which==39) {
+      console.log("right");
+    };
+    if(e.which==40) {
+      console.log("down");
+    };
+    if(e.which==37) {
+      console.log("left");
+    };
+  });
 });
